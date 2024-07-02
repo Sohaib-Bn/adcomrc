8d818f056d6e9523fb7056342083797587baecfd
Sohaib bn <87204575+Sohaib-Bn@users.noreply.github.com>
2024-07-02 11:56:33 +0200

    fix some bugs

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,28 +6,28 @@ function Dashboard() {
     <div className="relative h-screen">
       <div>
         <img
-          className="h-[18rem] 2xl:h-[22rem] absolute left-0 top-0"
-          src="/public/Untitled-5.png"
+          className="h-[16rem] 2xl:h-[20rem] absolute left-0 top-0"
+          src="/Untitled-5.png"
           alt="bg"
         />
         <img
-          className="h-[5rem] 2xl:h-[6rem] absolute right-[6.5rem] top-[4rem] 2xl:top-[4.3rem]"
-          src="/public/Untitled-6.png"
+          className="h-[4.5rem] 2xl:h-[6rem] absolute right-[6.5rem] top-[4rem] 2xl:top-[4.3rem]"
+          src="/Untitled-6.png"
           alt="bg"
         />
         <img
           className="h-[110%] absolute right-0 top-0"
-          src="/public/Untitled-3.png"
+          src="/Untitled-3.png"
           alt="bg"
         />
         <img
-          className="h-[2.2rem] 2xl:h-[2.5rem] absolute right-[8rem] 2xl:right-[5.8rem] bottom-[2.5rem] 2xl:bottom-[6rem]"
-          src="/public/Untitled-7.png"
+          className="h-[2rem] 2xl:h-[2.3rem] absolute right-[8rem] 2xl:right-[5.8rem] bottom-[2.5rem] 2xl:bottom-[6rem]"
+          src="/Untitled-7.png"
           alt="bg"
         />
         <img
-          className="h-[1.6rem] 2xl:h-[1.8rem] absolute left-[8rem] bottom-[4rem] 2xl:bottom-[7rem]"
-          src="/public/Untitled-4.png"
+          className="h-[1.4rem] 2xl:h-[1.6rem] absolute left-[8rem] bottom-[4rem] 2xl:bottom-[7rem]"
+          src="/Untitled-4.png"
           alt="bg"
         />
       </div>
@@ -35,14 +35,14 @@ function Dashboard() {
         <header className="flex items-center justify-center px-3 py-[3.5rem] 2xl:py-[4.2rem]">
           <Link to="/login">
             <img
-              className="h-[5.5rem] 2xl:h-[6.2rem]"
-              src="/public/logo.png"
+              className="h-[5.2rem] 2xl:h-[6rem]"
+              src="/logo.png"
               alt="logo"
             />
           </Link>
         </header>
         <main className="flex-1 px-[9.5rem] py-[1rem] 2xl:py-[2rem] flex items-center">
-          <div className="grid grid-cols-4 gap-x-12 gap-y-16 2xl:gap-y-20 w-full">
+          <div className="grid grid-cols-4 gap-x-10 2xl:gap-x-12 gap-y-16 2xl:gap-y-20 w-full">
             {DEPARTEMENT.map((dep) => (
               <DepartmentLink key={dep.label} data={dep} />
             ))}
@@ -50,7 +50,7 @@ function Dashboard() {
         </main>
         <footer className="flex items-center justify-center px-3 p-[4.2rem]">
           <div className="border-b-8 border-colorBrand leading-[1]">
-            <h1 className="uppercase font-bold 2xl:text-[5rem] text-[4.5rem] text-colorGreyText">
+            <h1 className="uppercase font-bold 2xl:text-[4.8rem] text-[4.3rem] text-colorGreyText">
               system
             </h1>
           </div>
@@ -68,12 +68,12 @@ function DepartmentLink({ data: { link, label } }) {
       target="_blank"
       rel="noreferrer"
       href={link}
-      className="flex uppercase flex-col items-center gap-2 justify-center border-2 border-colorGreyText py-4 px-3 text-colorGreyText hover:bg-colorBrand transition-all hover:text-colorWhite hover:border-colorBrand hover:-translate-y-2 hover:shadow-xl focus:shadow-sm focus:-translate-y-1"
+      className="flex uppercase flex-col items-center gap-2 justify-center border-[1.5px] border-colorGreyText py-4 px-3 text-colorGreyText hover:bg-colorBrand transition-all hover:text-colorWhite hover:border-colorBrand hover:-translate-y-2 hover:shadow-xl focus:shadow-sm focus:-translate-y-1"
     >
-      <span className="font-extrabold text-[1.8rem] 2xl:text-[2rem] leading-[1]">
+      <span className="font-extrabold text-[1.6rem] 2xl:text-[1.8rem] leading-[1]">
         {label}
       </span>
-      <span className="text-[1.8rem] 2xl:text-[1.7rem] font-light leading-[1]">
+      <span className="text-[1.5rem] 2xl:text-[1.5rem] font-light leading-[1]">
         center
       </span>
     </a>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,41 +26,41 @@ function Login() {
     <div className="flex items-center justify-center h-screen">
       <div>
         <img
-          className="h-[14rem] 2xl:h-[18rem] absolute left-0 top-0"
-          src="/public/Untitled-1.png"
+          className="h-[13rem] 2xl:h-[14rem] absolute left-0 top-0"
+          src="/Untitled-1.png"
           alt="bg"
         />
         <img
           className="h-screen absolute right-0 top-0"
-          src="/public/Untitled-3.png"
+          src="/Untitled-3.png"
           alt="bg"
         />
         <img
-          className="h-[1.4rem] 2xl:h-[1.6rem] absolute left-[14rem] bottom-[5rem] "
-          src="/public/Untitled-4.png"
+          className="h-[1.4rem] 2xl:h-[1.4rem] absolute left-[11rem] bottom-[4rem] "
+          src="/Untitled-4.png"
           alt="bg"
         />
       </div>
-      <div className="relative z-10 grid grid-cols-2 items-center gap-x-60 justify-center px-48 h-screen w-full">
+      <div className="relative z-10 grid grid-cols-2 items-center gap-x-60 justify-center px-40 2xl:px-48 h-screen w-full">
         <img
           className="h-[22rem] 2xl:h-[26rem] "
-          src="/public/Untitled-2.png"
+          src="/Untitled-2.png"
           alt="bg"
         />
         <div>
-          <div className="min-h-[30rem] w-[85%]">
-            <h1 className="uppercase text-colorGreyText text-[4.7rem] font-bold text-center mb-10">
+          <div className="min-h-[30rem] 2xl:w-[75%]">
+            <h1 className="uppercase text-colorGreyText text-[4.2rem] 2xl:text-[4.7rem] font-bold text-center mb-10 -translate-x-7">
               login
             </h1>
             <form onSubmit={handleSubmit} className="flex flex-col gap-14">
-              <div className="border-b-[2.5px] border-b-colorBrand pb-5 text-xl relative">
+              <div className="border-b-[2.5px] border-b-colorBrand pb-4 text-xl relative">
                 <label
                   className={`font-medium text-colorGreyText absolute transition-all ${
                     isUsernameFocused
                       ? "text-[1.3rem] translate-y-[-27px]"
                       : username
                       ? "text-[1.3rem] translate-y-[-27px]"
-                      : "text-[1.6rem]"
+                      : "text-[1.55rem]"
                   }`}
                 >
                   Username
@@ -73,9 +73,9 @@ function Login() {
                   type="text"
                   value={username}
                 />
-                <span className="absolute right-0 top-2 text-colorGreyText">
+                {/* <span className="absolute right-0 top-2 text-colorGreyText">
                   <FaUser />
-                </span>
+                </span> */}
               </div>
               <div className="border-b-[2.5px] border-b-colorBrand pb-5 text-xl relative">
                 <label
@@ -84,7 +84,7 @@ function Login() {
                       ? "text-[1.3rem] translate-y-[-27px]"
                       : password
                       ? "text-[1.3rem] translate-y-[-27px]"
-                      : "text-[1.6rem]"
+                      : "text-[1.55rem]"
                   }`}
                 >
                   Password
@@ -97,9 +97,9 @@ function Login() {
                   className="outline-0 relative z-10 bg-transparent text-2xl"
                   type="password"
                 />
-                <span className="absolute right-0 top-2 text-colorGreyText text-md">
+                {/* <span className="absolute right-0 top-2 text-colorGreyText text-md">
                   <FaLock />
-                </span>
+                </span> */}
                 {!isAuthenticated && (
                   <p className="font-midium text-[1.1rem] absolute -bottom-10 pl-2 text-colorError">
                     Password is incorrect
@@ -107,7 +107,7 @@ function Login() {
                 )}
               </div>
 
-              <button className="mt-[0.9rem] rounded-full w-[80%] m-auto p-4 bg-colorBrand font-semibold text-3xl text-colorWhite transition-all hover:bg-colorBrandHover">
+              <button className="mt-[0.5rem] rounded-full w-[75%] m-auto px-4 py-3 bg-colorBrand font-semibold text-[2rem] text-colorWhite transition-all hover:bg-colorBrandHover">
                 Login
               </button>
             </form>
